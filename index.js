ea246ca91111e6da8b8f3485fe9e9951a118b079
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-07-07 17:20:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.set('json spaces', 2)
 
+app.use((req, res, next) => {
+  clearTmp()
+  next()
+})
+
 app.get('/', (req, res) => {
   res.json({ message: 'still in development, i guess...' })
 })
@@ -27,7 +32,7 @@ app.post('/imagetopdf', async (req, res) => {
     let buffer = await toPDF(images)
     console.log(images.length, filename)
     await fs.writeFileSync(path.join(tmpFolder, filename), buffer)
-    res.json(await uploadFile(filename))
+    res.json(await uploadFile(path.join(tmpFolder, filename)))
   } catch (e) {
     res.json({ message: String(e) })
   }
@@ -36,6 +41,19 @@ app.post('/imagetopdf', async (req, res) => {
 const listener = app.listen(process.env.PORT || ~~(Math.random() * 1e4), () => {
   console.log('App running on port', listener.address().port)
 })
+
+function clearTmp() {
+  let filename = []
+  fs.readdirSync(tmpFolder).forEach(file => filename.push(path.join(tmpFolder, file)))
+  return filename.map(file => {
+    let stats = fs.statSync(file)
+    if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) {
+      console.log('Deleted file', file)
+      return fs.unlinkSync(file)
+    }
+    return false
+  })
+}
 
 async function toPDF(images, opt = {}) {
   return new Promise(async (resolve, reject) => {
