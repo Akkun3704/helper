b5d9a76263893ac5b4dad6b69bb21df82d0564a6
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-07-07 16:28:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const fs = require('fs')
 const jimp = require('jimp')
+const path = require('path')
 const axios = require('axios')
 const morgan = require('morgan')
 const express = require('express')
-const PDFDocument = require('pdfkit')
 const FormData = require('form-data')
+const PDFDocument = require('pdfkit')
 const bodyParser = require('body-parser')
 
+const tmpFolder = path.join(__dirname, './tmp')
 const app = express()
 
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.set('json spaces', 2)
 
+app.use((req, res, next) => {
+  clearTmp()
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.json({ message: 'i don\'t have idea what i\'am doing' })
+  res.json({ message: 'still in development, i guess...' })
 })
 
 app.post('/imagetopdf', async (req, res) => {
   try {
     let { images, filename } = req.body
-    if (!images) return res.json({ success: false, msg: 'Required an image url or buffer' })
+    if (!images) return res.json({ message: 'Required an image url' })
     if (!(filename && filename.endsWith('pdf'))) filename = `${~~(Math.random() * 1e9)}.pdf`
     let buffer = await toPDF(images)
     console.log(images.length, filename)
-    await fs.writeFileSync(process.cwd() + `/${filename}`, buffer)
+    await fs.writeFileSync(path.join(tmpFolder, filename), buffer)
     res.json(await uploadFile(filename))
   } catch (e) {
     res.json({ message: String(e) })
@@ -39,12 +46,26 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function clearTmp() {
+  let filename = []
+  fs.readdirSync(tmpFolder).forEach(file => filename.push(path.join(tmpFolder, file)))
+  return filename.map(file => {
+    let stats = fs.statSync(file)
+    if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) {
+      console.log('Deleted file', file)
+      return fs.unlinkSync(file)
+    }
+    return false
+  })
+}
+
 async function toPDF(images, opt = {}) {
   return new Promise(async (resolve, reject) => {
     if (!Array.isArray(images)) images = [images]
     let buffs = [], doc = new PDFDocument({ autoFirstPage: false })
     for (let image of images) {
-      let data = Buffer.isBuffer(image) ? image : (await axios.get(image, { responseType: 'arraybuffer', ...opt })).data
+      // let data = Buffer.isBuffer(image) ? image : (await axios.get(image, { responseType: 'arraybuffer', ...opt })).data
+      let data = (await axios.get(image, { responseType: 'arraybuffer', ...opt })).data
       let { width, height } = (await jimp.read(data)).bitmap
       doc.addPage({ size: [width, height] })
       doc.image(data, 0, 0)
@@ -64,11 +85,14 @@ async function uploadFile(path) {
   let res = await axios({
     url: 'https://uguu.se/upload.php',
     method: 'post',
+    data: form,
     headers: {
       ...form.getHeaders()
     },
-    data: form
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity
   })
   // await fs.promises.unlink(path)
-  return res.data.files[0]
+  if (res.data.success) return res.data.files[0]
+  return res.data
 }
