a17b9d3815fd5abf8fabe44c341a94e6f481625e
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-08-14 17:12:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -145,16 +145,20 @@ function clearTmp() {
 	})
 }
 
-async function toPDF(images, opt = {}) {
-	if (!Array.isArray(images)) images = [images]
-	let doc = new PDFDocument({ margin: 0, size: 'A4' })
-	for (let x = 0; x < images.length; x++) {
-		let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data
-		doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' })
-		if (images.length != x + 1) doc.addPage()
-	}
-	doc.end()
-	return doc
+function toPDF(images, opt = {}) {
+	return new Promise(async (resolve, reject) => {
+		if (!Array.isArray(images)) images = [images]
+		let buffs = [], doc = new PDFDocument({ margin: 0, size: 'A4' })
+		for (let x = 0; x < images.length; x++) {
+			let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data
+			doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' })
+			if (images.length != x + 1) doc.addPage()
+		}
+		doc.on('data', (chunk) => buffs.push(chunk))
+		doc.on('end', () => resolve(Buffer.concat(buffs)))
+		doc.on('error', (err) => reject(err))
+		doc.end()
+	})
 }
 
 async function nhentaiScraper(id) {
