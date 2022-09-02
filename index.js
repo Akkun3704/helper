493b445f71768e11b96efe80f1d282a41ee9279d
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-09-03 05:41:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.get('/doujindesu/:type', async (req, res) => {
 		} else if (/^download$/i.test(req.params.type)) {
 			if (!req.query.url) return res.json({ message: 'Required doujindesu url' })
 			let data = await doujindesuScraper('download', req.query.url)
-			let buffer = await toPDF(data.pages), filename = `${encodeURIComponent(data.title)}.pdf`
+			let buffer = await toPDF(data.pages), filename = `${data.title}.pdf`
 			fs.writeFileSync(path.join(tmpFolder, filename), buffer)
 			return res.json({ result: `https://${req.get('host')}/download/${filename}` })
 		}
